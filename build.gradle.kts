ce35857492a057df1095fa083a759f837e13370c
i.mikulik <i.mikulik@kpda.ru>
2022-10-03 14:59:12 +0300

    build.gradle: add lets-plot dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,11 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+
+    implementation("org.jetbrains.lets-plot:lets-plot-common:2.4.0")
+    implementation("org.jetbrains.lets-plot:lets-plot-kotlin-jvm:4.0.0")
+    implementation("org.jetbrains.lets-plot:lets-plot-image-export:2.4.0")
+    implementation("org.slf4j:slf4j-simple:2.0.1")
 }
 
 tasks.test {
